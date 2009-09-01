38223b32f2b44d6a8d5aa6b94bf638a2df5c041e
Erik Kastman <erik@nelson.medicine.wisc.edu>
2009-09-01 09:25:17 -0500

    Adding deploy:symlink_shared to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,21 @@ set :mongrel_pid, "tmp/pids/mongrel.pid"
 
 
 namespace :deploy do
-
+  # task :update_code
+  # task :symlink
+  
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "rm -rf #{release_path}/public/assets"
+    run "cp #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+    run "rm -rf #{release_path}/config/database.yml"
+    #run "rm -rf #{release_path}/images/*"
+    
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{release_path}/db/production.sqlite3 #{shared_path}/db/production.sqlite3"
+    #run "ln -nfs #{shared_path}/images #{release_path}/public/images"
+  end
   desc "Start Mongrels processes and add them to launchd."
   task :start, :roles => :app do
     mongrel_ports.each do |port|
@@ -47,5 +61,7 @@ namespace :deploy do
   end
  
 end
+
+after 'deploy:symlink', 'deploy:symlink_shared'
 
 
