015c79522ce7c655286b94618d0328fb109c3648
Erik Kastman <erik@nelson.medicine.wisc.edu>
2010-02-04 12:28:43 -0600

    Update to use Cucumber 0.6.2

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,5 @@
+# Basic UI Steps
+
+# -config.gem "thoughtbot-factory_girl",
+# -           :lib    => "factory_girl",
+# -           :source => "http://gems.github.com"
---
+++ features/support/cucumber_local_config.rb
@@ -0,0 +1,6 @@
+# require 'thoughtbot-factory_girl'
+# # config.gem "thoughtbot-factory_girl",
+# #   :lib    => "factory_girl",
+# #   :source => "http://gems.github.com"
+# 
+require "#{Rails.root}/spec/factories"
