ea9ee669c85a882dc688bbfb322a7252b923c9ed
Jennifer Oh <oh@jimbo.medicine.wisc.edu>
2010-03-18 12:54:26 -0500

    Added "Moderate" to image quality checks and pulled "Physiology Files" off of image dataset show page.

--- app/models/image_dataset_quality_check.rb
+++ app/models/image_dataset_quality_check.rb
@@ -1,6 +1,6 @@
 class ImageDatasetQualityCheck < ActiveRecord::Base
   PASSING_STATUSES = Set.new(%w(complete pass))
-  FAILING_STATUSES = Set.new( ["Incomplete","Mild","Severe","Limited Activation","No activation"] )
+  FAILING_STATUSES = Set.new( ["Incomplete","Mild","Moderate","Severe","Limited Activation","No activation"] )
   belongs_to :user
   belongs_to :image_dataset
   
