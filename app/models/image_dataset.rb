a4fe90cf05890337d1a2c888f8736c57b7463a16
Erik Kastman <erik@rafiki.medicine.wisc.edu>
2010-05-03 17:06:11 -0500

    Add regenerate thumbnails rake task.

--- app/models/image_dataset.rb
+++ app/models/image_dataset.rb
@@ -12,7 +12,7 @@ class ImageDataset < ActiveRecord::Base
   has_many :image_dataset_quality_checks, :dependent => :destroy
   has_one :log_file
   
-  has_attached_file :thumbnail, :styles => { :large => "900x900", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :thumbnail, :styles => { :large => "900x900>", :medium => "300x300>", :thumb => "100x100>" }
 
   
   validates_presence_of :path, :scanned_file
@@ -80,6 +80,28 @@ class ImageDataset < ActiveRecord::Base
   
   def find_by_dataset_identifier(path, scanned_file)
     self.class.find()
+  end
+  
+  def create_thumbnail
+    # Only available for Dicoms - Done through glob.
+    raise StandardError, "#{scanned_file} is not a DICOM image." if (scanned_file =~ /P.*\.7/ || scanned_file =~ /^I\./ || glob == nil)
+    
+    if File.exist?(File.join(path, scanned_file))
+      original_zip_status = false
+      file_to_scan = File.join(path, scanned_file)
+    elsif File.exist?("#{File.join(path, scanned_file)}.bz2") # Then scanned file is currently zipped
+      original_zip_status = true
+      file_to_scan = Pathname.new("#{File.join(path, scanned_file)}.bz2").local_copy.to_s
+    else
+      raise StandardError, "Could not find file #{File.join(path, scanned_file)} on filesystem."
+    end
+
+    ds = RawImageDataset.new(path, RawImageFile.new(file_to_scan))
+    
+    thumbnail = RawImageDatasetThumbnail.new(ds).create_thumbnail
+    raise StandardError, "Could not create thumbnail for #{File.join(path, scanned_file)}" unless File.exists?(thumbnail)
+    
+    return thumbnail
   end
   
   private
