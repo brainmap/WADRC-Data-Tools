089b6fa4b9d662161aa2e4e41a55bd10e7a3e5af
Erik Kastman <erik@nelson.medicine.wisc.edu>
2011-03-04 15:28:26 -0600

    Add user_spec

---
+++ spec/unit/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+  it "should authenticate with matching username and password" do
+    user = Factory(:user, :login => 'admin', :password => 'secret')
+    User.authenticate('admin', 'secret').should == user
+  end
+  
+  it "should not authenticate with incorrect password" do
+    user = Factory(:user, :login => 'admin', :password => 'secret')
+    User.authenticate('admin', 'incorrect').should be_nil
+  end
+end
