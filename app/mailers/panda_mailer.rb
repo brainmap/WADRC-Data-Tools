fe331b62c095a75c3d714fe05688e1d071618291
Erik <ekk@medicine.wisc.edu>
2011-07-28 17:16:29 -0500

    Fix Mail error message with strings and symbols in email_params hash

--- app/mailers/panda_mailer.rb
+++ app/mailers/panda_mailer.rb
@@ -3,6 +3,7 @@ class PandaMailer < ActionMailer::Base
   default :from => "noreply_johnson_lab@medicine.wisc.edu"
   
   def visit_confirmation(visit, email_params)
+    email_params.symbolize_keys!
     raise(StandardError, "Unable to send to external email addresses without email credentials.") if !email_params['send_to'].end_with?('@medicine.wisc.edu') unless self.class.smtp_credentialed?
     @visit = visit
     @username = visit.created_by.login if visit.created_by
