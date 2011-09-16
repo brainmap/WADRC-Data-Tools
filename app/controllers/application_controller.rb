2531fee96794023d6115563fa3c7e6fde461eefe
Erik <ekk@medicine.wisc.edu>
2011-09-16 18:25:06 -0500

    Require Login, Hide APOE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   include AuthenticatedSystem
 
-  before_filter :login_required, :only => [:edit, :update, :new, :create ]
+  before_filter :login_required #, :only => [:edit, :update, :new, :create ]
   #before_filter { |c| User.current_user = c.current_user }
   
 end
