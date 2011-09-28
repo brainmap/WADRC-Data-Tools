14cea84038c3ad455e280c693ea2d9049b4135d7
Erik <ekk@medicine.wisc.edu>
2011-09-28 17:12:52 -0500

    Fix metamri list_visit denials.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
 
-  include AuthenticatedSystem
+  # include AuthenticatedSystem
 
   before_filter :authorize #, :only => [:edit, :update, :new, :create ]
   #before_filter { |c| User.current_user = c.current_user }
   
+  private 
+  
+  # deny_access unless signed_in? or format is "xml"
+  def authorize
+    super unless params[:format] == 'xml'
+  end
 end
