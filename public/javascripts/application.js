30f34f10581b7492d5a9850599646daeaa110959
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2011-12-27 11:20:13 -0600

    trying to get toggle 2 to work in matser

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// not using the inner html text -- using an image
+// so can ignore the link div id
+function toggle2(showHideDiv, switchTextDiv) {
+	var ele = document.getElementById(showHideDiv);
+//	var text = document.getElementById(switchTextDiv);
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+		//text.innerHTML = "restore" ;
+  	}
+	else {
+		ele.style.display = "block";
+		//text.innerHTML = "collapse";
+	}
+}
