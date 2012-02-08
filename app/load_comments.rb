143c4d818d21994432f74e2acec92cdd7e81c745
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2012-02-08 13:29:18 -0600

    started on adding a load radiology comment rb

---
+++ app/load_comments.rb
@@ -0,0 +1,6 @@
+class LoadComments < ActiveRecord::Base
+  # This script will load the radiology_comments rad_path, the comment html, the comment_text, and set the q1_flag
+
+   puts "=============HELLLLLOOOOOOO================================="
+
+end
