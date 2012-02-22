6b8ac72a7d343c27c95132fb64c4e88075fcbe8e
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2012-02-22 11:46:29 -0600

    added comment_text_ and q1_flag population to daily load

--- app/load_comments.rb
+++ app/load_comments.rb
@@ -7,5 +7,6 @@ class LoadComments < ActiveRecord::Base
    # radiology_comments[0].
    radiology_comments[0].load_paths(1)
     radiology_comments[0].load_comments(1)
+    radiology_comments[0].load_text
    puts "======= finished path and comments load ====="
 end
