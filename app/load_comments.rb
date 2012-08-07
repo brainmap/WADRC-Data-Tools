aed0de98ea26b9f8bbc39d2e2a4d5d6827a825c1
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2012-08-07 13:47:32 -0500

    no change, just added space?

--- app/load_comments.rb
+++ app/load_comments.rb
@@ -5,9 +5,10 @@ class LoadComments < ActiveRecord::Base
   v_past_date = past_time.strftime("%Y-%m-%d")
    puts "=============starting path and comments load================================="
     radiology_comments  = RadiologyComment.find(:all)
+    
    # radiology_comments[0].  seems to only get the first visit????
     # going off of visit, ok if only called once   
-   radiology_comments[0].load_paths(1)
+    radiology_comments[0].load_paths(1)
 #   radiology_comments = RadiologyComment.where(" trim(radiology_comments.rad_path) is not null and  (radiology_comments.comment_html_1 is null
 #                  OR radiology_comments.comment_header_html_1 is null
 #                  OR radiology_comments.visit_id in (select visits.id from visits where visits.date >  '"+v_past_date+"' )  ) " )
