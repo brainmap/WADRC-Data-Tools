c42b5661306414dee429fa733c2aa95162a91bdb
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2012-10-10 18:29:50 -0500

    make show mri, pet, lh, lp, q, np into partials, and also called partials from vgroup show with hide/show toggle -- q not getting all questions -- toggle defaulting to open ( want close)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,6 +16,20 @@ function toggle2(showHideDiv, switchTextDiv) {
 	}
 }
 
+
+function toggle2_closed(showHideDiv, switchTextDiv) {
+	var ele = document.getElementById(showHideDiv);
+//	var text = document.getElementById(switchTextDiv);
+	if(ele.style.display == "none") {
+    		ele.style.display = "block";
+		//text.innerHTML = "restore" ;
+  	}
+	else {
+		ele.style.display = "none";
+		//text.innerHTML = "collapse";
+	}
+}
+
 function togglecomment(showHideDiv, switchTextDiv,first_text,second_text) {
 	var ele = document.getElementById(showHideDiv);
 	var text = document.getElementById(switchTextDiv);
