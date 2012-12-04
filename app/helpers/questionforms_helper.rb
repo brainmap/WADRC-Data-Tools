ef80b32781458b83c77cb73cebc10a50359a3577
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2012-12-04 14:18:43 -0600

    added required = true to text field and text area in q_data questions warning in vgroup creation about sp checkbox being required

--- app/helpers/questionforms_helper.rb
+++ app/helpers/questionforms_helper.rb
@@ -1,10 +1,15 @@
 module QuestionformsHelper
-  def draw_text_field(p_question_id, p_value_number,p_size,p_default_value, p_value,p_js)
+  def draw_text_field(p_question_id, p_value_number,p_size,p_default_value, p_value,p_js,p_required_y_n)
+
     p_js_array = p_js.split(",")
     
      if p_js_array.include?("numeric")
-        text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value) , :size=>p_size, :onchange => "if ( isNaN(this.value)){alert('The field needs to be all numeric.');this.value =''}"
-                                                                                
+       if p_required_y_n == "required"
+          text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value), :required=>TRUE, :size=>p_size, :onchange => "if ( isNaN(this.value)){alert('The field needs to be all numeric.');this.value =''}"
+      else
+         text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value), :size=>p_size, :onchange => "if ( isNaN(this.value)){alert('The field needs to be all numeric.');this.value =''}"
+        
+      end                                                                              
      elsif p_js_array.include?("calculated_bmi")
             text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value) , :size=>p_size, 
             :onchange => "if ( isNaN(this.value)){alert('The field needs to be all numeric.');this.value ='';t3 =document.getElementById('value_3_"+p_question_id.to_s+"_');t3.value=''}
@@ -18,12 +23,20 @@ module QuestionformsHelper
                                  t3 =document.getElementById('value_3_"+p_question_id.to_s+"_');t3.value= Math.round(t2.value/((0.01*t1.value)*(0.01*t1.value)))}"
          
     else
-         text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value) , :size=>p_size 
+         if p_required_y_n ==  "required"
+             text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value) , :required=>TRUE,:size=>p_size 
+         else
+            text_field_tag "value_"+p_value_number+"["+p_question_id.to_s+"][]",(p_value.try(:strip) or p_default_value) ,:size=>p_size 
+         end
       end
   end
   
-  def draw_text_area(p_question_id, p_value_number,p_size,p_default_value, p_value)
-    text_area_tag("value_"+p_value_number+"["+@question.id.to_s+"][]",(p_value.try(:strip) or p_default_value),:size => p_size) 
+  def draw_text_area(p_question_id, p_value_number,p_size,p_default_value, p_value, p_required_y_n)
+    if p_required_y_n ==  "required"
+      text_area_tag("value_"+p_value_number+"["+@question.id.to_s+"][]",(p_value.try(:strip) or p_default_value),:size => p_size, :required =>TRUE)
+    else
+      text_area_tag("value_"+p_value_number+"["+@question.id.to_s+"][]",(p_value.try(:strip) or p_default_value),:size => p_size) 
+    end
   end
   
 
