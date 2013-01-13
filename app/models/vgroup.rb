90313ac244e1ed41371127a0c5433818432458db
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2013-01-13 11:59:54 -0600

    re-edit

--- app/models/vgroup.rb
+++ app/models/vgroup.rb
@@ -59,13 +59,13 @@ class Vgroup < ActiveRecord::Base
          @v_start_id = p_start_id
          @v_end_id = p_end_id
          @vgroups = Vgroup.where( " id between "+@v_start_id+" and "+@v_end_id ).where("( nii_file_count is null or nii_file_count = 0 )")
-         v_group = ""
+         v_vgroup = nil
          @vgroups.each do |vg|
            if !vg.blank?
              v_vgroup = vg
            end
          end
-         v_base_path = @vgroups[0].get_base_path
+         v_base_path = v_vgroup.get_base_path
          v_glob = '*.nii'
          @vgroups.each do |vg|
             # could be multiple sp
