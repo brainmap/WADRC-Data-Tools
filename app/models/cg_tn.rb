194ef8f5ee23e80feefbde0bf7ac1b166355383c
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2013-01-17 17:45:24 -0600

    added datadictionary upload and links for the cg_tns tables

--- app/models/cg_tn.rb
+++ app/models/cg_tn.rb
@@ -1,4 +1,8 @@
 class CgTn < ActiveRecord::Base
    has_many :cg_tn_cns,:dependent => :destroy
    has_and_belongs_to_many :users
+   
+   has_attached_file :datadictionary
+   validates_attachment_size :datadictionary, :less_than => 50.megabytes
+   validates_attachment_content_type :datadictionary, :content_type => ['application/pdf','application/xls']
 end
