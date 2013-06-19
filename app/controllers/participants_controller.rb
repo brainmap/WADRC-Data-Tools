73d3e057252586269d4ae09057a408a13b0d2921
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2013-06-19 16:44:27 -0500

    added group list to participants show

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -41,6 +41,8 @@ class ParticipantsController < ApplicationController
      @participant = Participant.where(" participants.id in ( select participant_id from enrollments where enrollments.id in (select enrollment_vgroup_memberships.enrollment_id from enrollment_vgroup_memberships, scan_procedures_vgroups
            where enrollment_vgroup_memberships.vgroup_id = scan_procedures_vgroups.vgroup_id and scan_procedures_vgroups.scan_procedure_id in (?))) ", scan_procedure_array).find(params[:id])
 
+     @vgroups = Vgroup.where("participant_id in ( select participant_id from enrollments where participant_id = ? and enrollments.id in (select enrollment_vgroup_memberships.enrollment_id from enrollment_vgroup_memberships, scan_procedures_vgroups
+                  where enrollment_vgroup_memberships.vgroup_id = scan_procedures_vgroups.vgroup_id and scan_procedures_vgroups.scan_procedure_id in (?))) ", params[:id],scan_procedure_array)
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @participant }
