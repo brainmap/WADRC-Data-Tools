7626439519a34e24aa847f7287d8dfe328504da0
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2013-08-28 15:06:44 -0500

    adding more xls-ish content_type for data dictionary/ paperclip

--- app/models/cg_tn.rb
+++ app/models/cg_tn.rb
@@ -4,5 +4,5 @@ class CgTn < ActiveRecord::Base
    
    has_attached_file :datadictionary
    validates_attachment_size :datadictionary, :less_than => 50.megabytes
-   validates_attachment_content_type :datadictionary, :content_type => ['application/pdf','application/xls']
+   validates_attachment_content_type :datadictionary, :content_type => ["application/pdf","application/xls","application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
 end
