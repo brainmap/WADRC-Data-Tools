91e97c8b221c6396e3b496d2a1c4e8ff4a31182e
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2013-09-19 09:46:54 -0500

    antuano upload - added schedule_id to export_id, set done_flag

--- app/models/shared.rb
+++ app/models/shared.rb
@@ -616,7 +616,7 @@ puts "AAAAAA "+v_call
       sql_vgroup = "select DATE_FORMAT(max(v.vgroup_date),'%Y%m%d' ) from vgroups v where v.id in (select evm.vgroup_id from enrollment_vgroup_memberships evm, enrollments e where evm.enrollment_id = e.id and e.enumber ='"+r[0]+"')"
       results_vgroup = connection.execute(sql_vgroup)
       # mkdir /tmp/adrc_upload/[subjectid]_YYYYMMDD_wisc
-      v_export_id = r[3].to_s
+      v_export_id = ()@schedule.id).to_s+"_"+r[3].to_s
       v_subject_dir = v_export_id+"_"+(results_vgroup.first)[0].to_s+"_wisc"
       v_parent_dir_target =v_target_dir+"/"+v_subject_dir
       v_call = "ssh panda_admin@merida.dom.wisc.edu 'mkdir "+v_parent_dir_target +"' "
@@ -701,6 +701,8 @@ puts "AAAAAA "+v_call
       stderr.close
       
       # fsftp dir when set
+      sql_done = "update cg_antuano_20130916 set done_status ="Y" where subjectid = '"+r[0]+"'"
+      results_done = connection.execute(sql_done)
   
     end # results
     
