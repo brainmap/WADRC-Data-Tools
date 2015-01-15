2e3af0969d7344f8b8da9ba005e81422e5771cda
cai888 <caillingworth@medicine.wisc.edu>
2015-01-15 10:47:35 -0600

    up load page in routes

--- config/routes.rb
+++ config/routes.rb
@@ -207,6 +207,7 @@ WADRCDataTools::Application.routes.draw do
   match '/cg_tables' , :controller => 'data_searches', :action => 'cg_tables', :as =>:cg_tables
   match '/cg_edit_table/:id' , :controller => 'data_searches', :action => 'cg_edit_table', :as =>:cg_edit_table
     match '/cg_table_create_db' , :controller => 'data_searches', :action => 'cg_create_table_db', :as =>:cg_create_table_db
+     match '/cg_up_load' , :controller => 'data_searches', :action => 'cg_up_load', :as =>:cg_up_load
       match '/cg_table_edit_db' , :controller => 'data_searches', :action => 'cg_edit_table_db', :as =>:cg_edit_table_db
   match '/schedulerun_search' , :controller => 'scheduleruns', :action => 'schedulerun_search', :as =>:schedulerun_search
   match '/shared_file_upload' , :controller => 'shared', :action => 'file_upload', :as =>:file_upload
