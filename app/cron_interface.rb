e2ddf3efb4e9bd34c0709f91a7bc9c8e0273aff5
cai888 <caillingworth@medicine.wisc.edu>
2017-03-08 15:16:47 -0600

    paid export call

--- app/cron_interface.rb
+++ app/cron_interface.rb
@@ -179,6 +179,13 @@ v_user = v_user.gsub("\n","")
       v_shared = Shared.new
       v_shared.run_padi_upload()
 
+    elsif v_value_1 == "padi_upload_20170227_dicom"
+      v_shared = Shared.new
+      v_shared.run_padi_upload_20170227_dicom()
+    elsif v_value_1 == "padi_upload_20170227_nifty"
+      v_shared = Shared.new
+      v_shared.run_padi_upload_20170227_nifty())
+
   elsif v_value_1 == "padi_dvr_acpc_ids"
       v_shared = Shared.new
       v_shared.run_padi_dvr_acpc_ids()
