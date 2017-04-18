69c959c142400cd8cf27228937d8814159ecf7a0
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2017-04-18 11:36:08 -0500

    env not setting

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,13 @@ require File.expand_path('../application', __FILE__)
 WADRCDataTools::Application.initialize!
 
 require 'ruport/acts_as_reportable'   
-
+ ## not seem to do anything
 ENV['PATH']="/usr/local/rvm/gems/ruby-2.2.3@global/bin:/usr/local/rvm/rubies/ruby-2.2.3/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:~/bin:/bin:/sbin:/usr/local"
 ENV['GEM_HOME']="/usr/local/rvm/gems/ruby-2.2.3@global"
 ENV['GEM_PATH']="/usr/local/rvm/gems/ruby-2.2.3@global"
-ENV['RUBY_VERSION']="ruby-2.2.3"
+ENV['RUBY_VERSION']="ruby-2.2.3p173"
 # Bundler depends on this to use the gem version of metamri
-ENV['RAILS_ENV']="production" # development
+ENV['RAILS_ENV']="production" # development   
+ENV['MY_RUBY_HOME']="/usr/local/rvm/rubies/ruby-2.2.3" 
+
+
