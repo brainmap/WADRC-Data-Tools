bbb6aaefd6457fb4f155603125fb6fa583056c8e
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2017-04-18 10:14:24 -0500

    passing env values

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 WADRCDataTools::Application.initialize!
 
-require 'ruport/acts_as_reportable' 
+require 'ruport/acts_as_reportable'   
+
+ENV['PATH']="/usr/local/rvm/gems/ruby-2.2.3@global/bin:/usr/local/rvm/rubies/ruby-2.2.3/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:~/bin:/bin:/sbin:/usr/local"
+ENV['GEM_HOME']="/usr/local/rvm/gems/ruby-2.2.3@global"
+ENV['GEM_PATH']="/usr/local/rvm/gems/ruby-2.2.3@global"
+ENV['RUBY_VERSION']="ruby-2.2.3"
+# Bundler depends on this to use the gem version of metamri
+ENV['RAILS_ENV']="production" # development
