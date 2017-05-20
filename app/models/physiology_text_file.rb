209421f945b4a56c66fc18dd22cf62a0da8fd0fa
Chuck Illingworth <caillingworth@medicine.wisc.edu>
2017-05-20 12:36:47 -0500

    hashed out validation - not being used?

--- app/models/physiology_text_file.rb
+++ app/models/physiology_text_file.rb
@@ -3,6 +3,8 @@ class PhysiologyTextFile < ActiveRecord::Base
   validates_uniqueness_of :filepath
   
   # validates_format_of :filepath, :with => /^\/Data\/vtrak1\/raw\//, :message => "must begin with '/Data/vtrak1/raw'"
-  validates_format_of :filepath, :with => /^\/Volumes\/team*\/raw\//, :message => "must begin with '/Volumes/team*/raw'"
+  # not being used? wrong path even  causing error in startup  
+  #Message from application: The provided regular expression is using multiline anchors (^ or $), which may present a security risk. Did you mean to use \A and \z, or forgot to add the :multiline => true option? (ArgumentError) 
+  ### validates_format_of :filepath, :with => /^\/Volumes\/team*\/raw\//, :message => "must begin with '/Volumes/team*/raw'"
 
 end
