bac675c9e5e1f610409a7d1d8cf04b6cb47a04bd
Chuck A Illingworth <caillingworth@DOM9285.local>
2017-11-16 12:08:02 -0600

    Series description maps search - added scan procedures, like series_description and non-mapped

--- app/controllers/series_description_maps_controller.rb
+++ app/controllers/series_description_maps_controller.rb
@@ -13,18 +13,36 @@ class SeriesDescriptionMapsController < ApplicationController
   end    
   def series_description_map_search
       puts "dddddd"
-     if params[:series_description_map_search].nil?  
-       puts "ffff NIL"
-          params[:series_description_map_search] =Hash.new 
-       else
-         puts "gggggg "+params[:series_description_map_search][:series_description_type_id].join(", ")
-     end  
-     if !params[:series_description_map_search].nil? and !params[:series_description_map_search][:series_description_type_id][0].blank? 
-       puts "hhhhhhhh"
+     #if params[:series_description_map_search].nil?  
+      # puts "ffff NIL"
+      #    params[:series_description_map_search] =Hash.new 
+      # else
+      #   puts "gggggg "+params[:series_description_map_search][:series_description_type_id].join(", ")
+     # end  
+     if !params[:series_description_map_search].nil? and !params[:series_description_map_search][:series_description_type_id].nil? and !params[:series_description_map_search][:scan_procedure_id].nil? and !params[:series_description_map_search][:series_description_type_id][0].blank?  and !params[:series_description_map_search][:scan_procedure_id][0].blank? 
+  @series_description_maps = SeriesDescriptionMap.where("series_description_type_id in (?) and trim(series_description_maps.series_description) in (select trim(series_descriptions.long_description) from series_descriptions, series_description_scan_procedures where series_descriptions.id = series_description_scan_procedures.series_description_id  and series_description_scan_procedures.scan_procedure_id in (?))",params[:series_description_map_search][:series_description_type_id],params[:series_description_map_search][:scan_procedure_id])
+     
+
+     elsif !params[:series_description_map_search].nil? and !params[:series_description_map_search][:series_description_type_id].nil? and !params[:series_description_map_search][:series_description_type_id][0].blank? 
        @series_description_maps = SeriesDescriptionMap.where("series_description_type_id in (?)",params[:series_description_map_search][:series_description_type_id])
+     elsif !params[:series_description_map_search].nil? and !params[:series_description_map_search][:scan_procedure_id].nil? and !params[:series_description_map_search][:scan_procedure_id][0].blank? 
+       @series_description_maps = SeriesDescriptionMap.where("trim(series_description_maps.series_description) in (select trim(series_descriptions.long_description) from series_descriptions, series_description_scan_procedures where series_descriptions.id = series_description_scan_procedures.series_description_id  and series_description_scan_procedures.scan_procedure_id in (?))",params[:series_description_map_search][:scan_procedure_id])
      else
        @series_description_maps = SeriesDescriptionMap.all
      end
+
+     if !params[:series_description_map_search].nil? and !params[:series_description_map_search][:series_description].blank?
+           @series_description_maps = @series_description_maps.where("trim(series_description_maps.series_description) like '%"+params[:series_description_map_search][:series_description]+"%'")
+     end
+     if !params[:series_description_map_search].nil?  and (params[:series_description_map_search][:unmapped_series_descriptions] == "1")
+      #and !params[:series_description_map_search][:unmapped_series_descriptions].try(:length).nil?
+      puts "rrrrrr == 1"
+           @series_description_maps = @series_description_maps.where("series_description_maps.series_description_type_id is NULL")
+     end
+
+     puts "DDDDDD ="+params[:series_description_map_search][:unmapped_series_descriptions]+"="
+
+
      respond_to do |format|
        format.html   
        format.xml  { render :xml => @series_description_maps }
