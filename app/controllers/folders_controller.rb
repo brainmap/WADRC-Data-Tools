a41955078826900b6a1c7ad9ef05029548690924
Chuck A Illingworth <caillingworth@kubo.local>
2018-11-07 16:34:48 -0600

    Harvesting folder permission

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -70,33 +70,67 @@ class FoldersController < ApplicationController
          # insert new folderpermissions
 
          v_call = "ssh "+v_user+"@"+v_computer+".dom.wisc.edu 'getfacl "+folder.folder_path+"' "
-        puts "aaaaa="+v_call    
+        puts "aaaaa="+v_call  
+        v_val = "" 
         stdin, stdout, stderr = Open3.popen3(v_call)
         while !stdout.eof?
-          puts stdout.read 1024    
+          v_val =stdout.read 1024    
          end
         stdin.close
         stdout.close
         stderr.close
-
+        puts v_val
+        v_owner ="" ## owner:    default:user::
+        v_owner_group = "" ## group   default:group::
+        v_flags = "" # flags:
+        v_mask_permission = ""
+        v_other_permission =""
+        v_group_array = []
+        v_user_array = []
+        v_group_permission_hash = Hash.new
+        v_user_permission_hash = Hash.new
+        # default:user:<user_name>:rwx
+        # default:group:<group_name>:rwx
+        # default:mask::rwx    ???
+        #default:other::
+        v_row_array = v_val.split(/\n+/)
+        v_row_array.each do |v_row|
+          if v_row.start_with?("# owner:")
+                v_owner = (v_row.split(":"))[1].gsub(/\s+/, "")
+          elsif v_row.start_with?("# group:")
+                v_owner_group = (v_row.split(":"))[1].gsub(/\s+/, "")
+          elsif v_row.start_with?("default:user::")
+                v_user_permission_hash[v_owner] = v_row.split(":")[3]
+          elsif v_row.start_with?("default:group::")
+                v_group_permission_hash[v_owner_group] = v_row.split(":")[3]
+          elsif v_row.start_with?("mask::")
+                v_mask_permission = v_row.split(":")[2]
+          elsif v_row.start_with?("default:user:") and !v_row.start_with?("default:user::")
+                v_tmp_array = v_row.split(":")
+                v_user_array.push(v_tmp_array[2])
+                v_user_permission_hash[v_tmp_array[2]] = v_tmp_array[3]
+          elsif v_row.start_with?("default:group:") and !v_row.start_with?("default:group::")
+                v_tmp_array = v_row.split(":")
+                v_group_array.push(v_tmp_array[2])
+                v_group_permission_hash[v_tmp_array[2]] = v_tmp_array[3]
+          elsif v_row.start_with?("default:other::")
+                v_other_permission = v_row.split(":")[3]
+          end
+        end
+        # save in folderpermissions
 
      end
-
   end
 
   def folder_home
-    #####self.collect_folder_actual_permissions
+    self.collect_folder_actual_permissions
      @v_next_one_hash = Hash.new
      @v_next_two_hash = Hash.new
      @v_next_three_hash = Hash.new
      @v_next_four_hash = Hash.new
      @v_next_five_hash = Hash.new
-     @v_next_one_count_hash = Hash.new
-     @v_next_two_count_hash = Hash.new
-     @v_next_three_count_hash = Hash.new
-     @v_next_four_count_hash = Hash.new
-     @v_next_five_count_hash = Hash.new
-     @v_top_count_hash = Hash.new
+     
+     @v_top_count_hash = Hash.new # used to define colspan in parent cells/table 
 
      @top_folder = Folder.where("folders.parent_id not in ( select id from folders)")
      @top_folder.each do |top|
@@ -104,31 +138,26 @@ class FoldersController < ApplicationController
        @next_one_folder = Folder.where("parent_id in (?)",top.id)
        if !@next_one_folder.nil?
           @v_next_one_hash[top.id] = @next_one_folder
-          @v_next_one_count_hash[top.id] = @next_one_folder.count
           @v_top_count_hash[top.id] = @next_one_folder.count
           @next_one_folder.each do |nextfol_one|   
              @next_two_folder = Folder.where("parent_id in (?)",nextfol_one.id)
              if !@next_two_folder.nil? and @next_two_folder.count > 0
                  @v_next_two_hash[nextfol_one.id] = @next_two_folder
-                 @v_next_two_count_hash[nextfol_one.id] = @next_two_folder.count
                  @v_top_count_hash[top.id] = @next_two_folder.count
                  @next_two_folder.each do |nextfol_two|   
                    @next_three_folder = Folder.where("parent_id in (?)",nextfol_two.id)
                    if !@next_three_folder.nil? and @next_three_folder.count > 0
                      @v_next_three_hash[nextfol_two.id] = @next_three_folder
-                     @v_next_three_count_hash[nextfol_two.id] = @next_three_folder.count
                      @v_top_count_hash[top.id] = @next_three_folder.count
                      @next_three_folder.each do |nextfol_three|   
                        @next_four_folder = Folder.where("parent_id in (?)",nextfol_three.id)
                        if !@next_four_folder.nil? and @next_four_folder.count > 0
                          @v_next_four_hash[nextfol_three.id] = @next_four_folder
-                         @v_next_four_count_hash[nextfol_three.id] = @next_four_folder.count
                          @v_top_count_hash[top.id] = @next_four_folder.count
                          @next_four_folder.each do |nextfol_four|   
                            @next_five_folder = Folder.where("parent_id in (?)",nextfol_four.id)
                            if !@next_five_folder.nil? and @next_five_folder.count > 0
                              @v_next_five_hash[nextfol_four.id] = @next_five_folder
-                             @v_next_five_count_hash[nextfol_four.id] = @next_five_folder.count
                              @v_top_count_hash[top.id] = @next_five_folder.count
                            end # five not nil      
                          end # loop four
