f962c9560a7902e001453bffa54336604cf9da7f
Chuck A Illingworth <caillingworth@kubo.local>
2018-12-13 09:52:21 -0600

    Cleaned out the cg_query, tn, tn_cn loops, starting from scratch to build sql

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -25,14 +25,36 @@ class DashboardsController < ApplicationController
   # POST /dashboards.json
   def dashboard_home
 
-      @conditions = []
-      @joins = []
-      @tables = ['vgroups']
-      @columns = []
+      scan_procedure_list = (current_user.view_low_scan_procedure_array).split(' ').map(&:to_i).join(',')
+      @scan_procedure_list = scan_procedure_list
+      v_secondary_key_tn_array =[]
+      @local_column_headers =[]
+      @local_fields = []
+      @local_conditions =[]
+      @conditions_bak = []
+      @local_tables =[] # need to add outer join to table, -- 
+      @local_tables_alias_hash =Hash.new # need to make pet tracer select -- tracker?
+      @tables_left_join_hash = Hash.new
+      @tables_secondary_key_join_hash = Hash.new
+      @joins = [] # just inner joins
+      @cg_query_tn_id_array = []
+      @cn_p_vg_array = [] # used to flag column as participant specific
+      @cg_query_tn_hash = Hash.new
+      @cg_query_tn_cn_hash = Hash.new
+      @cg_query_cn_hash = Hash.new
+      @cg_query_tn_not_in_hash = Hash.new
+      @add_cg_tn_id = []
       @current_tab = "cg_search"
       params["search_criteria"] =""
       @participant_key_results_hash = Hash.new  # use particiapnt_id and dashboardcontent_id both as keys for results -multi column
       @participant_key_columns_hash = Hash.new  # use particiapnt_id and dashboardcontent_id both as keys for column display names from cg_tn_cn
+
+      # might go away
+
+      @conditions = [] # for inital search input
+      @tables = [] # for inital search input
+      @columns = [] # for inital search input
+
     @dashboards = Dashboard.where("status_flag ='Y' ").order(:display_name)
 
     if params[:dashboard_home].nil?
@@ -95,65 +117,81 @@ class DashboardsController < ApplicationController
      # for now just assumming many=participant
 
     if  !params[:dashboard_home].blank? and !params[:dashboard_home][:id].blank?
-       @dashboard = Dashboard.find(params[:dashboard_home][:id])
-       @participants = ""
-       if @dashboard.one_or_many_results == 'many' # assume partipant
+      @dashboard = Dashboard.find(params[:dashboard_home][:id])
+      @participants = ""
+      if @dashboard.one_or_many_results == 'many' # assume partipant
            if !v_reggieid.blank?
              @participants = Participant.where("reggieid in ('"+v_reggieid+"'")
            elsif !v_wrapnum.blank?
              @participants = Participant.where("wrapnum in ('"+v_wrapnum+"'")
            end
-       end
-       @dashboardcontents = Dashboardcontent.where("dashboardcontents.dashboard_id in (?)", @dashboard.id).order(:row_display_order)
+      end
+      @dashboardcontents = Dashboardcontent.where("dashboardcontents.dashboard_id in (?)", @dashboard.id).order(:row_display_order)
        # loop thru each dashboardcontent
-       @joins_hash = Hash.new
-       @tables_hash = Hash.new
-       @columns_hash = Hash.new
-       @dashboardcontents.each do |content_row|
-          v_cg_tn = ""
-          v_cg_tn_cn = ""
-          if !content_row.cg_query_id.blank?
-            v_cg_query = CgQuery.find(content_row.cg_query_id)
+      @joins_hash = Hash.new
+      @tables_hash = Hash.new
+      @columns_hash = Hash.new
+      @dashboardcontents.each do |content_row|
+        # need clear out arrays each loop
+        @all_tables = []
+        @all_table_ids_in_query = []
+        @order_by = []
+        @pet_tracer_array = []
+        @scan_procedures = []
+        @sp_array = []
+        @table_types = []
+        v_cg_tn_array = []
+        if !content_row.cg_query_id.blank?
+          @cg_query = CgQuery.find(content_row.cg_query_id)
             # from line 1277 data_searches_controller
-            if !@cg_query.scan_procedure_id_list.blank?
-                @sp_array = @cg_query.scan_procedure_id_list.split(",")
-            end
-            if !@cg_query.pet_tracer_id_list.blank?
-                 @pet_tracer_array = @cg_query.pet_tracer_id_list.split(",")
-            end
-
-            @cg_query_tns =  CgQueryTn.where("cg_query_id = "+@cg_query.id.to_s).where("cg_tn_id in ( select cg_tns.id from cg_tns where cg_tns.table_type in 
-              (SElect table_type from cg_table_types where cg_table_types.protocol_id is null or cg_table_types.protocol_id in (select scan_procedures.protocol_id from scan_procedures where scan_procedures.id in ("+scan_procedure_list+"))))").order("cg_query_tns.display_order")  
-        
-            @cg_query_tns.each do |cg_query_tn|
-              v_tn_id = cg_query_tn.cg_tn_id 
-              @cg_query_tn_id_array.push(v_tn_id) # need to retrieve for display on the page
-              # if cg table needd to propagate from search to search
-              v_cg_tn = CgTn.find(v_tn_id)
-              if v_cg_tn.table_type != 'base'  
-       puts "zzzzz add_table id = "+v_tn_id.to_s 
-                @add_cg_tn_id.push(v_tn_id.to_s) 
-              end
-              @cg_query_tn_hash[v_tn_id.to_s] = cg_query_tn  
-              @cg_query_tn_cns = CgQueryTnCn.where("cg_query_tn_id = "+cg_query_tn.id.to_s) 
-              @cg_query_tn_cns.each do |cg_query_tn_cn|
-              v_tn_cn_id = cg_query_tn_cn.cg_tn_cn_id.to_s
-              @cg_query_cn_hash[v_tn_cn_id] = cg_query_tn_cn
-            end 
-            @cg_query_tn_cn_hash[v_tn_id.to_s] = @cg_query_cn_hash         
+          if !@cg_query.scan_procedure_id_list.blank?
+            @sp_array = @cg_query.scan_procedure_id_list.split(",")
+          end
+          if !@cg_query.pet_tracer_id_list.blank?
+            @pet_tracer_array = @cg_query.pet_tracer_id_list.split(",")
           end
 
-            # assemble columns, tables, joins, conditions from CgQuery parts - 
-            # add in report specific conditions - 2526 line in data_searches_controller
+          @cg_query_tns =  CgQueryTn.where("cg_query_id = "+@cg_query.id.to_s).where("cg_tn_id in ( select cg_tns.id from cg_tns where cg_tns.table_type in 
+              (select table_type from cg_table_types where cg_table_types.protocol_id is null or cg_table_types.protocol_id in (select scan_procedures.protocol_id from scan_procedures where scan_procedures.id in ("+scan_procedure_list+"))))").order("cg_query_tns.display_order")  
+          @cg_query_tns.each do |cg_query_tn|
+            # join_type, include_tn, display_order
+            @cg_tn = CgTn.find(cg_query_tn.cg_tn_id)
+             # tn, join_left, join_right, join_left_parent_tn, secondary_key_flag, display_order, alias, tracker_id, view_tn_participant_link, table_type, table_group_id   
+# build up joins
+puts "table name="+@cg_tn.tn
+            @cg_query_tn_cns = CgQueryTnCn.where("cg_query_tn_cns.cg_query_tn_id in (?)",cg_query_tn.id)
+            @cg_query_tn_cns.each do |cg_query_tn_cn|
+              #value_1, value_2, condition, include_cn
+              @cg_tn_cn = CgTnCn.find(cg_query_tn_cn.cg_tn_cn_id)
+               # cn, cg_tn_id, export_name, key_column_flag, ref_table_a, ref_table_b, display_order, secondary_key_protocol_flag,secondary_key_visitno_flag, comma_parse_value,exclude_from_char_replacement_flag
+# build up select columns
+# build up conditions
+puts "column name ="+@cg_tn_cn.cn
 
-            #
-         end # ???? 
-
-
-        # get results 
+            end # loop thru cq_query_tn_cn
+          end # loop thru cg_query_tn
+# try to make order by only in selecvt so works on newer MySql
+ 
+          sql = " select distinct "+@local_fields.join(',')+" from "
     
-       end
-    end
+          @local_tables.uniq.each do |tn|   # need left join right after parent tn
+            v_tn = tn
+            if !@tables_left_join_hash[tn].blank?
+              v_tn = v_tn +" "+ @tables_left_join_hash[tn] 
+            end
+            @all_tables.push(v_tn)
+          end
+          sql = sql + @all_tables.uniq.join(", ")
+          sql = sql + " where "+ @local_conditions.uniq.join(" and ")
+          sql = sql+" order by "+@order_by.join(",")
+          puts "sql = "+sql
+          # loop participants # gets wierd with enumber/reggieis/wapnum in Dashboard saved query
+              # call sql with each participant id
+                # put results in hash [particiapnt_id][dashboard_content_id]
+
+        end # there is a cg_query_id  
+      end # dashboard contents loop
+    end # there is a dashboard id 
 
 
     respond_to do |format|
