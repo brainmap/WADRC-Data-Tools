d5febfac25bc754dd36cfbb45f012b336986dfdf
Bill Bevis <wbbevis@medicine.wisc.edu>
2019-05-13 15:19:19 -0500

    Codifying the visit abbreviation. Used mostly in shared.rb so far.

--- app/models/scan_procedure.rb
+++ app/models/scan_procedure.rb
@@ -5,4 +5,22 @@ class ScanProcedure < ActiveRecord::Base
   has_many :consent_form_scan_procedures,:dependent => :destroy
   #has_and_belongs_to_many :consent_form    -- was causing delete error - looking for consent_formS_scan_procedures
 
+  def visit_abbr(iv="")
+  	#because sometimes we want this to return "_v1"
+  	out = iv
+
+  	#check the codename to see if this is "visit2", "visit3", etc.
+  	(2..8).to_a.each do |x|
+  		if codename.include?("visit#{x}")
+  			out = "_v#{x}"
+  		end
+  	end
+
+  	#or if there's a special abbreviation for this visit
+  	if !visit_number_abbreviation.nil? and visit_number_abbreviation > "" and out == ""
+  		out = "_#{visit_number_abbreviation}"
+  	end 
+
+  	return out
+  end
 end
