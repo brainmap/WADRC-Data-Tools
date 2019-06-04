e2508af61f74a45f20e8cd86c6df30497551014f
Bill Bevis <wbbevis@medicine.wisc.edu>
2019-06-04 10:17:40 -0500

    AV45 DICOMs made autodetect difficult.

--- app/controllers/petscans_controller.rb
+++ app/controllers/petscans_controller.rb
@@ -976,60 +976,73 @@ injectiontime =  params[:date][:injectiont][0]+"-"+params[:date][:injectiont][1]
                         end
                         @petfile_header = nil
                         if !@petfiles_dicoms_found_array.nil? and !@petfiles_dicoms_found_array[0].nil? and !@petfiles_dicoms_found_array[1].nil?
+
                           @petfile_header = @petfiles_dicoms_found_array[1]
                           # GET FROM NEW FILE DIR STRUCTURE v_file_type = #######
-                           v_dicom_scanner_name = @petfile_header['0008,1090'][:value].to_s
-                           if v_dicom_scanner_name.include?("Discovery MI")
-                                params[:petscan][:scanner_name] = v_dicom_scanner_name
-                                @petscan.scanner_name = v_dicom_scanner_name
-                                @petscan.save
-                             v_dicom_tracer = @petfile_header['0008,103E'][:value].to_s
-                             #0008,103E Series Description  AV_1451 30 MIN DYN
-                             # need some interpretation
-                             if v_dicom_tracer.include?("PiB") # MK6240
-                                 v_dicom_tracer_id = 1
-                             elsif v_dicom_tracer.include?("FDG") # MK6240
-                                 v_dicom_tracer_id = 2
-                             elsif v_dicom_tracer.include?("WAY") # MK6240
-                                 v_dicom_tracer_id = 3
-                             elsif v_dicom_tracer.include?("PBR28") # MK6240
-                                 v_dicom_tracer_id = 5
-                             elsif v_dicom_tracer.include?("AV45") # MK6240
-                                 v_dicom_tracer_id = 6
-                             elsif v_dicom_tracer.include?("1451") # AV_1451
-                                 v_dicom_tracer_id = 9
-                             elsif v_dicom_tracer.include?("NeuraCeq") # MK6240
-                                 v_dicom_tracer_id = 10
-                             elsif v_dicom_tracer.include?("6240") # MK6240
-                                 v_dicom_tracer_id = 11
-                             elsif v_dicom_tracer.include?("UCB-J") # MK6240
-                                 v_dicom_tracer_id = 12 
+
+                          #especially on AV45 dicoms from WIMR(?), files named 9xx are summary files, and aren't appropriate 
+                          # for this copyforward we're doing onto the Petscan obj (they break stuff, and aren't what we want anyway).
+                          if !@petfiles_dicoms_found_array[0].to_s.start_with?('9')
+                             v_dicom_scanner_name = @petfile_header['0008,1090'][:value].to_s
+                             if v_dicom_scanner_name.include?("Discovery MI")
+                                  params[:petscan][:scanner_name] = v_dicom_scanner_name
+                                  @petscan.scanner_name = v_dicom_scanner_name
+                                  @petscan.save
+                               v_dicom_tracer = @petfile_header['0008,103E'][:value].to_s
+                               #0008,103E Series Description  AV_1451 30 MIN DYN
+                               # need some interpretation
+                               if v_dicom_tracer.include?("PiB") # MK6240
+                                   v_dicom_tracer_id = 1
+                               elsif v_dicom_tracer.include?("FDG") # MK6240
+                                   v_dicom_tracer_id = 2
+                               elsif v_dicom_tracer.include?("WAY") # MK6240
+                                   v_dicom_tracer_id = 3
+                               elsif v_dicom_tracer.include?("PBR28") # MK6240
+                                   v_dicom_tracer_id = 5
+                               elsif v_dicom_tracer.include?("AV45") # MK6240
+                                   v_dicom_tracer_id = 6
+                               elsif v_dicom_tracer.include?("1451") # AV_1451
+                                   v_dicom_tracer_id = 9
+                               elsif v_dicom_tracer.include?("NeuraCeq") # MK6240
+                                   v_dicom_tracer_id = 10
+                               elsif v_dicom_tracer.include?("6240") # MK6240
+                                   v_dicom_tracer_id = 11
+                               elsif v_dicom_tracer.include?("UCB-J") # MK6240
+                                   v_dicom_tracer_id = 12 
+                               end
+
+                               if @petfile_header.has_key?('0008,0201') and !@petfile_header['0008,0201'].nil? and !@petfile_header['0008,0201'].blank?
+                                 v_dicom_timezone_offset = @petfile_header['0008,0201'][:value].to_s
+                               else
+                                 v_dicom_timezone_offset = ""
+                               end
+                               #0008,0201 Timezone Offset From UTC  -0600
+                                # need to move the injection/scan start time to times in database WITH the GMT displacement
+                                  # running up against UTC displayed /GMT saved in database 
+                                  # and the petscan injection/scan start dates ignoring the GMT displacement
+                                  # need to -06 hours , but from hh - and if displayment > hh, need the 12 back wrap
+                               
+                               if @petfile_header.has_key?('0008,0031') and !@petfile_header['0008,0031'].nil? and !@petfile_header['0008,0031'].blank?
+                                 v_dicom_scan_start_time = @petfile_header['0008,0031'][:value].to_s
+                               end
+
+                               #0008,0031 Series Time. HHMMSS
+                               v_dicom_scanner_location = @petfile_header['0008,0080'][:value].to_s    # Institution Name
+                               if params[:petscan][:scanner_location].nil? or params[:petscan][:scanner_location].blank? or params[:petscan][:scanner_location].include?("elect")
+                                  params[:petscan][:scanner_location] = v_dicom_scanner_location
+                                  @petscan.scanner_location = v_dicom_scanner_location
+                                  @petscan.save
+                               end
+
+                               if @petfile_header.has_key?('0008,0020') and !@petfile_header['0008,0020'].nil? and !@petfile_header['0008,0020'].blank?
+                                 v_dicom_scan_date = @petfile_header['0008,0020'][:value].to_s
+                               end
+                               # raise warnings
+
+                             elsif  v_dicom_scanner_name.include?("Biograph Horizon")
+
+
                              end
-
-                             if @petfile_header.has_key?('0008,0201') and !@petfile_header['0008,0201'].nil? and !@petfile_header['0008,0201'].blank?
-                               v_dicom_timezone_offset = @petfile_header['0008,0201'][:value].to_s
-                             else
-                               v_dicom_timezone_offset = ""
-                             end
-                             #0008,0201 Timezone Offset From UTC  -0600
-                              # need to move the injection/scan start time to times in database WITH the GMT displacement
-                                # running up against UTC displayed /GMT saved in database 
-                                # and the petscan injection/scan start dates ignoring the GMT displacement
-                                # need to -06 hours , but from hh - and if displayment > hh, need the 12 back wrap
-                             v_dicom_scan_start_time = @petfile_header['0008,0031'][:value].to_s
-                             #0008,0031 Series Time. HHMMSS
-                             v_dicom_scanner_location = @petfile_header['0008,0080'][:value].to_s    # Institution Name
-                             if params[:petscan][:scanner_location].nil? or params[:petscan][:scanner_location].blank? or params[:petscan][:scanner_location].include?("elect")
-                                params[:petscan][:scanner_location] = v_dicom_scanner_location
-                                @petscan.scanner_location = v_dicom_scanner_location
-                                @petscan.save
-                             end
-                             v_dicom_scan_date = @petfile_header['0008,0020'][:value].to_s  # Study Date
-                             # raise warnings
-
-                           elsif  v_dicom_scanner_name.include?("Biograph Horizon")
-
-
                            end
                           
                            # what to do with dicom header
