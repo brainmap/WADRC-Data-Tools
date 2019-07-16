3956730690628ce961796fcd029f361aac732ea2
Bill Bevis <wbbevis@medicine.wisc.edu>
2019-07-16 09:13:47 -0500

    A little more logging.

--- config/initializers/warden_tagged_logger.rb
+++ config/initializers/warden_tagged_logger.rb
@@ -5,7 +5,9 @@ module WardenTaggedLogger
 			session_data = req.cookie_jar.encrypted[session_key]
 			warden_data = session_data["warden.user.user.key"]
 			warden_data[0][0]
-		rescue
+		rescue StandardError => e
+			logger.debug "Error finding the current user: #{e.message}"
+			logger.debug "Backtrace: #{e.backtrace.inspect}"
 			nil
 		end
 	end
