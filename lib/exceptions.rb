c2a01ad56d9c97e82f4da0c5f3482536c0bd10d3
Bill Bevis <wbbevis@medicine.wisc.edu>
2019-09-26 16:37:18 -0500

    This helps parallel petscan processing to run.

---
+++ lib/exceptions.rb
@@ -0,0 +1,10 @@
+module Exceptions
+
+	#Petscan
+	class PetscanError < StandardError; end
+	class PetscanPathError < PetscanError; end
+	class PetscanTooManyEcatsError < PetscanError; end
+	class PetscanNoEcatsError < PetscanError; end
+	class PetscanTooManyEcatsError < PetscanError; end
+
+end
