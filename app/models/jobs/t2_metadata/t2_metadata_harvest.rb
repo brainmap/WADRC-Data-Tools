c3bd48e3fa3e361075b199955b1cd026a8e2fa4a
Bill Bevis <wbevis@gmail.com>
2021-01-14 15:44:37 -0600

    this needs to not be called the same thing as the method that populates it

--- app/models/jobs/t2_metadata/t2_metadata_harvest.rb
+++ app/models/jobs/t2_metadata/t2_metadata_harvest.rb
@@ -68,7 +68,7 @@ class Jobs::T2Metadata::T2MetadataHarvest < Jobs::BaseJob
 	end
 
 	def selection(params)
-		@selection = ImageDataset.where("(series_description not like 'ORIG%') and ((series_description like 'SAG Cube T2 FLAIR%') or (series_description like 'Sag T2 FLAIR Cube%')  or (series_description like 'Sag CUBE T2FLAIR%') or (series_description like 'Sag CUBE flair%'))")
+		@selected = ImageDataset.where("(series_description not like 'ORIG%') and ((series_description like 'SAG Cube T2 FLAIR%') or (series_description like 'Sag T2 FLAIR Cube%')  or (series_description like 'Sag CUBE T2FLAIR%') or (series_description like 'Sag CUBE flair%'))")
 	end
 
 	def filter(params)
