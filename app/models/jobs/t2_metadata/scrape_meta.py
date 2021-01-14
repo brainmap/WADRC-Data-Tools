52327b8f7df1f43d22dedcc7c4d87038c0e851a4
Bill Bevis <wbevis@gmail.com>
2021-01-14 12:58:12 -0600

    Adding these for a metadata gathering job.

---
+++ app/models/jobs/t2_metadata/scrape_meta.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import pydicom, json
+from optparse import OptionParser
+
+usage = "usage: %prog [-f] filename"
+parser = OptionParser(usage)
+parser.add_option("-f", "--filename", dest="filename", type=str, help="Path to the file we're going to scrape")
+
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+	parser.error("incorrect number of arguments")
+
+filename = options.filename if options.filename else args[0]
+
+# This script is set up to look at some dicom headers, massage the values a little, and return a JSON report.
+# If anything goes wrong, return a JSON report that includes a description of the error.
+
+# column_mapping = [(0x0018, 0x0022), # Scan Options, should include 'FILTERED_GEMS' is this is PURE corrected
+# 	(0x0043, 0x102d), # Filter Mode', should have "P+", "p+", or "wp+" if this is PURE corrected
+# 	(0x0018,0x9021), # 'T2 Preparation', this may be blank :(
+# 	(0x0018,0x1250), # 'Receive Coil Name', from which I can get the number of channels
+# 	(0x0008,0x1090), # 'Manufacturer's mode name', which will become 'scanner_name'
+# ]
+
+# ["t2_prep", "pure_correction", "channel_count", "coil_name", "scanner"]
+
+dcm = pydicom.dcmread(filename)
+
+json = {}
+
+try:
+	scan_options = dcm[(0x0018,0x0022)] # Scan Options, should include 'FILTERED_GEMS' is this is PURE corrected
+	filter_mode = dcm[(0x0043,0x102d)] # Filter Mode', should have "P+", "p+", or "wp+" if this is PURE corrected
+
+	pure_corrected_status = 'False'
+
+	if ('FILTERED_GEMS' in scan_options.value) and (filter_mode.value in ["P+", "p+", "wp+"]):
+		pure_corrected_status = 'True'
+	elif ('FILTERED_GEMS' in scan_options.value) or (filter_mode.value in ["P+", "p+", "wp+"]):
+		pure_corrected_status = 'Other - (scan options is %s, filter mode is %s' % (','.join(scan_options.value), filter_mode.value)
+
+	t2_prep = dcm[(0x0018,0x9021)] # 'T2 Preparation', this may be blank :(
+	coil_name = dcm[(0x0018,0x1250)] # 'Receive Coil Name', from which I can get the number of channels
+	scanner = dcm[(0x0008,0x1090)] # 'Manufacturer's mode name', which will become 'scanner_name'
+
+	channel_count = 0
+	if coil_name == '8HRBRAIN':
+		channel_count = 8
+
+	json = {'t2_prep': t2_prep, 'pure_correction': pure_corrected_status, 'channel_count':channel_count, 'coil_name':coil_name, 'scanner':scanner}
+
+except Exception as e:
+	if 'error' not in json.keys():
+		json['error'] = []
+	json['error'].append "%s, %s" % (type(e), e)
+
+
+print(json.dumps(json))
+
---
+++ app/models/jobs/t2_metadata/t2_metadata_harvest.rb
@@ -0,0 +1,164 @@
+class Jobs::T2Metadata::T2MetadataHarvest < Jobs::BaseJob
+
+	require 'open3'
+	
+	attr_accessor :header
+	attr_accessor :error_rows
+	attr_accessor :connection
+	attr_accessor :total_scans_considered
+	attr_accessor :selected
+	attr_accessor :driver
+
+	def self.default_params
+	  	params = { :schedule_name => 'T2 Metadata Harvest', 
+	  				:base_path => "/mounts/data/raw",
+	  				:run_by_user => 'panda_user',
+	  				:target_table => 'cg_image_dataset_metadata',
+	  				:staging_dir => "/mounts/data/analyses/wbbevis/t2_metadata_staging",
+	  				:python_bin_dir => "/mounts/data/analyses/wbbevis/t2_metadata",
+	  				:write_sql_to_file => true,
+	  				:auto_insert => false
+	  			}
+        params.default = ''
+        params
+    end
+
+	def run(params)
+
+		begin
+			setup(params)
+
+			rotate_old(params)
+
+			selection(params)
+
+			filter(params)
+
+			harvest(params)
+
+			rotate_new(params)
+
+			close(params)
+		
+		rescue StandardError => error
+
+			self.error_log << "Error (#{error.class}): #{error.message}"
+			close_fail(params, error)
+
+		end
+	end
+
+	def setup(params)
+		@header = ["t2_prep", "pure_correction", "channel_count", "coil_name", "scanner"]
+		@error_rows = []
+		@connection = ActiveRecord::Base.connection
+
+		# set up the staging dir
+		if !File.exists? params[:staging_dir]
+			Dir.mkdir params[:staging_dir]
+		end
+	end
+
+	def rotate_old(params)
+		sql = "truncate table #{params[:target_table]}_old"
+		@connection.execute(sql)
+		sql = "insert into #{params[:target_table]}_old select * from #{params[:target_table]}"
+		@connection.execute(sql)
+	end
+
+	def selection(params)
+		@selection = ImageDataset.where("(series_description not like 'ORIG%') and ((series_description like 'SAG Cube T2 FLAIR%') or (series_description like 'Sag T2 FLAIR Cube%')  or (series_description like 'Sag CUBE T2FLAIR%') or (series_description like 'Sag CUBE flair%'))")
+	end
+
+	def filter(params)
+		# here we're going to populate @driver with images, and we want to be sure that all of the paths exist,
+		# that there are dicoms somewhere below the directory in path, etc. 
+
+		@selected.each do |image|
+
+			# how's the path?
+			if !File.exists? image.path or !File.directory? image.path
+				self.exclusions << {:class => image.class, :id => image.id, :message => "image has a bad path"}
+				next
+			end
+
+			# are there dicoms in there?
+			if Dir.glob("#{image.path}/*.bz2").count < 1 and Dir.glob("#{image.path}/*/*.bz2").count < 1
+				self.exclusions << {:class => image.class, :id => image.id, :message => "no dicoms found in this path"}
+				next
+			end
+
+			@driver << image
+		end
+	end
+
+	def harvest(params)
+
+		sql_outfile = nil
+		if params[:write_sql_to_file]
+			sql_outfile	= File.open(params[:sql_outfile], 'wb')
+		end
+
+		@driver.each do |image|
+			# here, we're going to try to copy the first dicom from the image's raw dir, unzip, and call
+			# the python script that will look into it and return us a JSON.
+
+
+			dicoms = Dir.glob("#{image.path}/*.bz2")
+			if dicoms.count == 0
+				dicoms = Dir.glob("#{image.path}/*/*.bz2")
+			end
+
+			first_dicom =  dicoms.first
+			r_call "cp #{first_dicom} #{params[:staging_dir]}/"
+			filename = first_dicom.split("/").last
+
+			json_response = r_call "source #{params[:python_bin_dir]}/bin/activate && python #{params[:python_bin_dir]}/t2_metadata.py #{params[:staging_dir]}/#{filename}"
+
+			# the JSON response will be either an error, or a complete report on the fields, or some other 
+			# error that doesn't parse as JSON.
+
+			parsed_json = JSON.parse(json_response)
+
+			if parsed_json["error"]
+				#record the error
+			else
+				# prepare this response for insert into the metadata table
+
+				columns = []
+				values = []
+				@header.each do |key|
+					columns << key
+					values << (parsed_json[key].nil? ? "NULL" : @connection.quote(parsed_json[key]))
+				end
+
+				sql = "INSERT INTO #{params[:target_table]} (image_dataset_id, path, #{columns.join(', ')}) values (#{image.id}, #{image.path}, #{values.join(', ')});"
+
+				if params[:write_sql_to_file]
+					sql.write("#{sql}\n")
+				end
+
+				if params[:auto_insert]
+					@connection.execute(sql)
+				end
+
+			end
+		end
+
+		if params[:write_sql_to_file]
+			sql_outfile.close
+		end
+
+	end
+
+	def rotate_new(params)
+		sql = "truncate table #{params[:target_table]}"
+		@connection.execute(sql)
+		sql = "insert into #{params[:target_table]} select * from #{params[:target_table]}_new"
+		@connection.execute(sql)
+	end
+
+	def report_errors(params)
+
+	end
+end
