b7f3d59075e332d415f1e16ff4069c21ef3e41b3
Bill Bevis <wbevis@gmail.com>
2021-11-09 10:00:10 -0600

    Adding neuraceq SUVR for WLS

--- app/cron_interface.rb
+++ app/cron_interface.rb
@@ -266,6 +266,11 @@ v_user = v_user.gsub("\n","")
      params = Pet::ParallelPetProcess.fdg_suvr_params
      job.run(params)
     
+   elsif v_value_1 == "parallel_pet_neuraceq_suvr_process"
+    
+     job = Pet::ParallelPetProcess.new
+     params = Pet::ParallelPetProcess.neuraceq_suvr_params
+     job.run(params)
 
    elsif v_value_1 == "parallel_pet_mk6240_process"
      job = Pet::ParallelPetProcess.new
