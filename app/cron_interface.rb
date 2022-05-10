3121e33343cba8a5021f32f968030fcf8d26c80f
Bill Bevis <wbevis@gmail.com>
2022-05-10 16:04:21 -0500

    Plumbing in ADRC LPs from REDCap

--- app/cron_interface.rb
+++ app/cron_interface.rb
@@ -460,6 +460,13 @@ v_user = v_user.gsub("\n","")
 
     job.run(params)
 
+  elsif v_value_1 == "adrc_lp_harvest"
+
+    params = Jobs::RemoteRequest::AdrcLpRequest.default_params
+    job = Jobs::RemoteRequest::AdrcLpRequest.new(params)
+
+    job.run(params)
+
   elsif v_value_1 == "xnat_curated_upload"
 
       xnat = SharedUpload::XnatCuratedUpload.new
